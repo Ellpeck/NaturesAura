5456262cf114115d4bff07876058cf9fed3cffa3
Ellpeck <megamaximal@gmail.com>
2018-11-08 21:35:32 +0100

    add actual hardness to the hopper upgrade

--- src/main/java/de/ellpeck/naturesaura/blocks/BlockHopperUpgrade.java
+++ src/main/java/de/ellpeck/naturesaura/blocks/BlockHopperUpgrade.java
@@ -1,10 +1,13 @@
 package de.ellpeck.naturesaura.blocks;
 
 import de.ellpeck.naturesaura.blocks.tiles.TileEntityHopperUpgrade;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 
 public class BlockHopperUpgrade extends BlockContainerImpl {
     public BlockHopperUpgrade() {
         super(Material.IRON, "hopper_upgrade", TileEntityHopperUpgrade.class, "hopper_upgrade");
+        this.setSoundType(SoundType.METAL);
+        this.setHardness(2.5F);
     }
 }
