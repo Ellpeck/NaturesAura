b20c1be95f2cd22ab79739a4732ba2b6a7d184cb
Ellpeck <megamaximal@gmail.com>
2018-11-08 13:50:45 +0100

    added bottled aura

---
+++ src/main/java/de/ellpeck/naturesaura/items/ItemAuraBottle.java
@@ -0,0 +1,88 @@
+package de.ellpeck.naturesaura.items;
+
+import de.ellpeck.naturesaura.NaturesAura;
+import de.ellpeck.naturesaura.aura.AuraType;
+import de.ellpeck.naturesaura.aura.chunk.AuraChunk;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.text.translation.I18n;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+public class ItemAuraBottle extends ItemImpl {
+
+    public ItemAuraBottle() {
+        super("aura_bottle");
+        this.addPropertyOverride(new ResourceLocation(NaturesAura.MOD_ID, "type"),
+                (stack, worldIn, entityIn) -> getType(stack).ordinal());
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onRightClick(PlayerInteractEvent.RightClickItem event) {
+        ItemStack held = event.getItemStack();
+        if (held.isEmpty() || held.getItem() != Items.GLASS_BOTTLE)
+            return;
+        EntityPlayer player = event.getEntityPlayer();
+        RayTraceResult ray = this.rayTrace(player.world, player, true);
+        if (ray != null && ray.typeOfHit == RayTraceResult.Type.BLOCK)
+            return;
+        BlockPos pos = player.getPosition();
+        if (AuraChunk.getAuraInArea(player.world, pos, 30) < 1000)
+            return;
+
+        if (!player.world.isRemote) {
+            held.shrink(1);
+
+            player.inventory.addItemStackToInventory(
+                    setType(new ItemStack(this), AuraType.forWorld(player.world)));
+
+            BlockPos spot = AuraChunk.getHighestSpot(player.world, pos, 30, pos);
+            AuraChunk.getAuraChunk(player.world, spot).drainAura(spot, 500);
+
+            player.world.playSound(null, player.posX, player.posY, player.posZ,
+                    SoundEvents.ITEM_BOTTLE_FILL_DRAGONBREATH, SoundCategory.PLAYERS, 1F, 1F);
+        }
+        player.swingArm(event.getHand());
+    }
+
+    @Override
+    public void getSubItems(CreativeTabs tab, NonNullList<ItemStack> items) {
+        if (this.isInCreativeTab(tab)) {
+            for (AuraType type : AuraType.values()) {
+                ItemStack stack = new ItemStack(this);
+                setType(stack, type);
+                items.add(stack);
+            }
+        }
+    }
+
+    @Override
+    public String getItemStackDisplayName(ItemStack stack) {
+        return I18n.translateToLocal(this.getUnlocalizedNameInefficiently(stack) + "." + getType(stack).name().toLowerCase() + ".name").trim();
+    }
+
+    public static AuraType getType(ItemStack stack) {
+        if (!stack.hasTagCompound())
+            return AuraType.OTHER;
+        String type = stack.getTagCompound().getString("type");
+        return AuraType.valueOf(type);
+    }
+
+    public static ItemStack setType(ItemStack stack, AuraType type) {
+        if (!stack.hasTagCompound())
+            stack.setTagCompound(new NBTTagCompound());
+        stack.getTagCompound().setString("type", type.name());
+        return stack;
+    }
+}
--- src/main/java/de/ellpeck/naturesaura/items/ModItems.java
+++ src/main/java/de/ellpeck/naturesaura/items/ModItems.java
@@ -9,6 +9,14 @@ import java.util.Locale;
 
 public final class ModItems {
 
+    public static final Item.ToolMaterial TOOL_MATERIAL_INFUSED_IRON =
+            EnumHelper.addToolMaterial(NaturesAura.MOD_ID.toUpperCase(Locale.ROOT) + "_INFUSED_IRON", 3, 300, 6.25F, 2.25F, 16);
+    public static final Item INFUSED_PICKAXE = new ItemPickaxeNA("infused_iron_pickaxe", TOOL_MATERIAL_INFUSED_IRON);
+    public static final Item INFUSED_AXE = new ItemAxeNA("infused_iron_axe", TOOL_MATERIAL_INFUSED_IRON, 8.25F, -3.2F);
+    public static final Item INFUSED_SHOVEL = new ItemShovelNA("infused_iron_shovel", TOOL_MATERIAL_INFUSED_IRON);
+    public static final Item INFUSED_HOE = new ItemHoeNA("infused_iron_hoe", TOOL_MATERIAL_INFUSED_IRON);
+    public static final Item INFUSED_SWORD = new ItemSwordNA("infused_iron_sword", TOOL_MATERIAL_INFUSED_IRON);
+
     public static final Item EYE = new ItemEye();
     public static final Item GOLD_FIBER = new ItemGoldFiber();
     public static final Item GOLD_LEAF = new ItemImpl("gold_leaf");
@@ -17,13 +25,6 @@ public final class ModItems {
     public static final Item COLOR_CHANGER = new ItemColorChanger();
     public static final Item AURA_CACHE = new ItemAuraCache();
     public static final Item SHOCKWAVE_CREATOR = new ItemShockwaveCreator();
-
-    public static final Item.ToolMaterial TOOL_MATERIAL_INFUSED_IRON =
-            EnumHelper.addToolMaterial(NaturesAura.MOD_ID.toUpperCase(Locale.ROOT) + "_INFUSED_IRON", 3, 300, 6.25F, 2.25F, 16);
-    public static final Item INFUSED_PICKAXE = new ItemPickaxeNA("infused_iron_pickaxe", TOOL_MATERIAL_INFUSED_IRON);
-    public static final Item INFUSED_AXE = new ItemAxeNA("infused_iron_axe", TOOL_MATERIAL_INFUSED_IRON, 8.25F, -3.2F);
-    public static final Item INFUSED_SHOVEL = new ItemShovelNA("infused_iron_shovel", TOOL_MATERIAL_INFUSED_IRON);
-    public static final Item INFUSED_HOE = new ItemHoeNA("infused_iron_hoe", TOOL_MATERIAL_INFUSED_IRON);
-    public static final Item INFUSED_SWORD = new ItemSwordNA("infused_iron_sword", TOOL_MATERIAL_INFUSED_IRON);
     public static final Item MULTIBLOCK_MAKER = new ItemMultiblockMaker();
+    public static final Item AURA_BOTTLE = new ItemAuraBottle();
 }
