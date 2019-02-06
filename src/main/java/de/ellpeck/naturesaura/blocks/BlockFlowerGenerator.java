36a18247b699757e1fc875dcb8bb60a76e381e84
Ellpeck <megamaximal@gmail.com>
2019-02-06 14:42:43 +0100

    give the flower gen a range display Closes #26

--- src/main/java/de/ellpeck/naturesaura/blocks/BlockFlowerGenerator.java
+++ src/main/java/de/ellpeck/naturesaura/blocks/BlockFlowerGenerator.java
@@ -1,14 +1,32 @@
 package de.ellpeck.naturesaura.blocks;
 
+import de.ellpeck.naturesaura.api.render.IVisualizable;
 import de.ellpeck.naturesaura.blocks.tiles.TileEntityFlowerGenerator;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class BlockFlowerGenerator extends BlockContainerImpl {
+public class BlockFlowerGenerator extends BlockContainerImpl implements IVisualizable {
 
     public BlockFlowerGenerator() {
         super(Material.WOOD, "flower_generator", TileEntityFlowerGenerator.class, "flower_generator");
         this.setSoundType(SoundType.WOOD);
         this.setHardness(2F);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public AxisAlignedBB getVisualizationBounds(World world, BlockPos pos) {
+        return new AxisAlignedBB(pos).grow(3);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public int getVisualizationColor(World world, BlockPos pos) {
+        return 0xffed2b;
     }
 }
