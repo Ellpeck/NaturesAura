6ccc6ab00582cadef271ba70f3afc50e025be00d
Ellpeck <megamaximal@gmail.com>
2019-02-25 13:37:48 +0100

    pls no crash server

--- src/main/java/de/ellpeck/naturesaura/blocks/tiles/TileEntityGeneratorLimitRemover.java
+++ src/main/java/de/ellpeck/naturesaura/blocks/tiles/TileEntityGeneratorLimitRemover.java
@@ -1,10 +1,13 @@
 package de.ellpeck.naturesaura.blocks.tiles;
 
 import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class TileEntityGeneratorLimitRemover extends TileEntityImpl {
 
     @Override
+    @SideOnly(Side.CLIENT)
     public AxisAlignedBB getRenderBoundingBox() {
         return new AxisAlignedBB(this.pos, this.pos.add(1, 2, 1));
     }
