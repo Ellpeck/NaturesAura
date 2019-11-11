38c645624635d5731778db285ac4a9a011c62471
JoshuaBright <cs8minty@gmail.com>
2019-11-11 18:10:52 +0900

    번역 완료

---
+++ src/main/resources/assets/naturesaura/patchouli_books/book/새 텍스트 문서.bat
@@ -0,0 +1,2 @@
+mklink /D /H "D:\mc-mod\NaturesAura\src\main\resources\assets\naturesaura\patchouli_books\book\ko_kr2" "D:\mc-mod\NaturesAura\src\main\resources\assets\naturesaura\patchouli_books\book\ko_kr"
+pause
---
+++ src/main/resources/assets/새 텍스트 문서.bat
@@ -0,0 +1,2 @@
+mklink /D "C:\Users\Bright\Documents\Curse\Minecraft\Instances\LocalizationPack\resources\na2" "D:\mc-mod\NaturesAura\src\main\resources\assets\naturesaura"
+pause
