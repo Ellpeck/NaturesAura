0d2f6f538971097fb4c3ccec1c3b64e5d31e8232
Ellpeck <me@ellpeck.de>
2024-11-20 21:48:20 +0100

    fixed fortress finder crashing servers closes #372

--- src/main/java/de/ellpeck/naturesaura/items/ItemStructureFinder.java
+++ src/main/java/de/ellpeck/naturesaura/items/ItemStructureFinder.java
@@ -18,13 +18,11 @@ public class ItemStructureFinder extends ItemImpl {
 
     private final ResourceKey<Structure> structure;
     private final int color;
-    private final int radius;
 
-    public ItemStructureFinder(String baseName, ResourceKey<Structure> structure, int color, int radius) {
+    public ItemStructureFinder(String baseName, ResourceKey<Structure> structure, int color) {
         super(baseName);
         this.structure = structure;
         this.color = color;
-        this.radius = radius;
     }
 
     @Override
@@ -34,7 +32,8 @@ public class ItemStructureFinder extends ItemImpl {
             var registry = levelIn.registryAccess().registryOrThrow(Registries.STRUCTURE);
             var holderSet = registry.getHolder(this.structure).map(HolderSet::direct).orElse(null);
             if (holderSet != null) {
-                var pos = ((ServerLevel) levelIn).getChunkSource().getGenerator().findNearestMapStructure((ServerLevel) levelIn, holderSet, playerIn.blockPosition(), this.radius, false);
+                // apparently the radius isn't really the radius anymore and even the locate command uses 100, which still allows it to find structures further away
+                var pos = ((ServerLevel) levelIn).getChunkSource().getGenerator().findNearestMapStructure((ServerLevel) levelIn, holderSet, playerIn.blockPosition(), 100, false);
                 if (pos != null) {
                     var entity = new EntityStructureFinder(ModEntities.STRUCTURE_FINDER, levelIn);
                     entity.setPos(playerIn.getX(), playerIn.getY(0.5D), playerIn.getZ());
